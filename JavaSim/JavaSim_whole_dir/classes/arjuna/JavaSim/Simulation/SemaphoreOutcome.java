d1d6ef051b93f7815570ee2501c0b3a6fbc76403
Mahesh Elireddy <mahesh961@gmail.com>
2011-11-16 00:05:48 -0500

    .

--- JavaSim/JavaSim_whole_dir/classes/arjuna/JavaSim/Simulation/SemaphoreOutcome.java
+++
@@ -1,8 +0,0 @@
-package arjuna.JavaSim.Simulation;
-
-public class SemaphoreOutcome
-{
-    static final public int DONE = 0;
-    static final public int NOTDONE = 1;
-    static final public int WOULD_BLOCK = 2;
-};
