51859517dd68d38d92056e927139c9f227a9e97c
walikazi <ahmed.waliullah.kazi@gmail.com>
2012-02-13 22:54:06 -0600

    Minor revision

--- JavaSim/src/com/simulator/ccn/CCNCache.java
+++ JavaSim/src/com/simulator/ccn/CCNCache.java
@@ -20,7 +20,7 @@ public class CCNCache  {
 	private Map<Integer, Packets> cache;
 	
 	/**
-	 * This is the max size upto which this cache can grow. 
+	 * This is the max size up to which this cache can grow. 
 	 * Before doing any inserts we check if the resultant cache size is bigger
 	 * than this limit we do a employ a replacement strategy to free up sapce. 
 	 */
@@ -40,14 +40,14 @@ public class CCNCache  {
 	
 	private float hashTableLoadFactor = 0.75f;
 	/**
-	 * Constructor of CCNCache which intilizes cache and sets maxsize to be
+	 * Constructor of CCNCache which initializes cache and sets max-size to be
 	 * 1000 and current size to zero.
 	 * @param id 
 	 */
-	public CCNCache(int id)
-	{
+	public CCNCache(int id)	{
+		
 		setNodeId(id);
-		setMaxSize(10000);
+		setMaxSize(100000);
 		setSizeOfCache(0);
 		setMaxEntries(10000);
 		Integer hashTableSize = (int) Math.ceil(getMaxEntries() / hashTableLoadFactor) +1 ;
@@ -55,63 +55,64 @@ public class CCNCache  {
 	}
 	/**
 	 * Adds a packet to the cache. First checks if the packet size can fit into
-	 * the cache by comparing the cahce size to max size. Else calls one of
+	 * the cache by comparing the cache size to max size. Else calls one of
 	 * the replacement strategies to make space for this packet in the cache.
 	 * @param packet the packet to be inserted in to the cache
 	 */
-	public void addToCache(Packets packet)
-	{
+	public void addToCache(Packets packet) {
+		
 		//While adding to cache change the origin node of the packet to this node.
 		packet.setOriginNode(getNodeId());
-		if(getSizeOfCache() + packet.getSizeOfPacket() < getMaxSize())
-		{
-			cache.put(packet.getPacketId(), packet); // just put the packet upand return
+		if(getSizeOfCache() + packet.getSizeOfPacket() < getMaxSize()) {
+			
+			cache.put(packet.getPacketId(), packet); // just put the packet up and return
 			setSizeOfCache(getSizeOfCache()+packet.getSizeOfPacket());
 			return;
 		}
 		// this is the else part where we need to free up memory
 		Iterator<Integer> itr = cache.keySet().iterator(); // This iterator gives me a LRU sorted list.
-		while(getSizeOfCache() + packet.getSizeOfPacket() > getMaxSize())
-		{
+		while(getSizeOfCache() + packet.getSizeOfPacket() > getMaxSize()) {
+			
 			Integer key = (Integer) itr.next();
 			setSizeOfCache(getSizeOfCache() - cache.get(key).getSizeOfPacket());
 			cache.remove(key);
 		}
 
 		cache.put(packet.getPacketId(), packet); // just put the packet and return
-		setSizeOfCache(getSizeOfCache()+packet.getSizeOfPacket());		
+		setSizeOfCache(getSizeOfCache()+packet.getSizeOfPacket());	
+		
 		return;
 	}
 	/**
 	 * Removes a packet from the cache. and update the size of the cache. 
 	 * @param packet
 	 */
-	public void removeFromCache(Packets packet)
-	{
-		if(cache.containsKey(packet.getPacketId()))
-		{
-		cache.remove(packet.getPacketId());
-		setSizeOfCache(getSizeOfCache() - packet.getSizeOfPacket());
+	public void removeFromCache(Packets packet) {
+		
+		if(cache.containsKey(packet.getPacketId()))	{
+			cache.remove(packet.getPacketId());
+			setSizeOfCache(getSizeOfCache() - packet.getSizeOfPacket());
 		}
+		
 		return;
 	}
 	
-	public boolean isPresentInCache(Packets packet)
-	{
+	public boolean isPresentInCache(Packets packet) {
 		return cache.containsKey(packet.getPacketId());
 	}
 	
 	@Override
-	public String toString()
-	{
+	public String toString() {
+		
 		String str;
 		str = "{ nodeId:"+getNodeId()+ " size:"+getSizeOfCache()+ "\nCache:"+cache.toString()+"}\n";
 		return str;
 	}
-	public Packets getaPacketFromCache(Integer pacektId)
-	{
+	
+	public Packets getaPacketFromCache(Integer pacektId) {
 		return cache.get(pacektId);
 	}
+	
 	public Integer getMaxSize() {
 		return maxSize;
 	}
@@ -122,18 +123,23 @@ public class CCNCache  {
 	public Integer getSizeOfCache() {
 		return sizeOfCache;
 	}
+	
 	public void setSizeOfCache(Integer sizeOfCache) {
 		this.sizeOfCache = sizeOfCache;
 	}
+	
 	public Integer getMaxEntries() {
 		return maxEntries;
 	}
+	
 	public void setMaxEntries(Integer maxEntries) {
 		this.maxEntries = maxEntries;
 	}
+	
 	public int getNodeId() {
 		return nodeId;
 	}
+	
 	public void setNodeId(int nodeId) {
 		this.nodeId = nodeId;
 	}
