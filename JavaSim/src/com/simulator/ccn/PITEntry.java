e73c2000ab199ca33c77a77459b2715915686d78
walikazi <ahmed.waliullah.kazi@gmail.com>
2012-04-03 08:14:31 -0500

    Addition of entry to specify which Interest packet (PkID) was associated with the PIT entry

--- JavaSim/src/com/simulator/ccn/PITEntry.java
+++ JavaSim/src/com/simulator/ccn/PITEntry.java
@@ -1,19 +1,33 @@
 package com.simulator.ccn;
 
+import java.util.Iterator;
+import java.util.List;
+
+import com.simulator.packets.Packets;
+
 public class PITEntry {
 	
 	private int outgoingInterface;
+	private int refPacketId;
 	private double createdAtTime;
 	
 	PITEntry () {
 		
 		outgoingInterface = 0;
-		createdAtTime = 0;		
+		createdAtTime = 0;	
+		refPacketId = 0;
 	}
 	
 	PITEntry (int tempInt, double tempTime) {
 		
 		outgoingInterface = tempInt;
+		createdAtTime = tempTime;		
+	}
+	
+	PITEntry (int tempInt, int tempPacketId, double tempTime) {
+		
+		outgoingInterface = tempInt;
+		refPacketId = tempPacketId;
 		createdAtTime = tempTime;		
 	}
 	
@@ -25,7 +39,8 @@ public class PITEntry {
 	        return false;
 	    
 	    PITEntry pn = (PITEntry)o;
-	    return pn.outgoingInterface == outgoingInterface;
+	    return pn.outgoingInterface == outgoingInterface 
+	    		&& pn.refPacketId == refPacketId;
 	}
 	
 	public int hashCode() {
@@ -33,9 +48,14 @@ public class PITEntry {
 	}
 	
 	
+	public void setRefPacketId (int temp) {
+		refPacketId = temp;
+	}
+	
 	public void setOutgoingInterface (int temp) {
 		outgoingInterface = temp;
 	}
+	
 	
 	public void setCreatedAtTime (double temp) {
 		createdAtTime = temp;
@@ -45,7 +65,11 @@ public class PITEntry {
 		return outgoingInterface;
 	}
 	
+	public int getRefPacketId () {
+		return refPacketId;
+	}
+	
 	public double getCreatedAtTime () {
 		return createdAtTime;
-	}
+	}			
 }
