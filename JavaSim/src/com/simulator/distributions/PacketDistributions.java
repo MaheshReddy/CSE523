a413886e6d7251fc1e54be8e9d293f2b4ebb9db4
walikazi <ahmed.waliullah.kazi@gmail.com>
2012-11-19 18:10:28 -0600

    Minor Revision: Adding circular distribution of objects in distributeContentGlobeTraff() function

--- JavaSim/src/com/simulator/distributions/PacketDistributions.java
+++ JavaSim/src/com/simulator/distributions/PacketDistributions.java
@@ -3,10 +3,7 @@ package com.simulator.distributions;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Random;
 import java.util.ArrayList;
-
-/* I am in the master at Wali's PC*/
 
 //import org.apache.log4j.Logger;
 
@@ -33,7 +30,7 @@ public class PacketDistributions {
 
 	private static String allDocs = null;
 	
-	private static Random nodeSelecter = new Random(5);
+	//private static Random nodeSelecter = new Random(5);
 	
 	/* This list is created to store the leaf nodes of a topology. This nodes in this list
 	 * will be used as the source and destination of all interest and data packets. 
@@ -44,8 +41,6 @@ public class PacketDistributions {
 	 * for segmentation of Interest packets
 	 * */
 	public static long size[];
-	
-
 	
 	/**
 	 *  distributeContentGlobeTraffic distributes the content from doc.web produced by globe traffic tool into various nodes of the topology.
@@ -144,13 +139,19 @@ public class PacketDistributions {
 		/* Open the docs.web file to read the object id's and their sizes */
 		BufferedReader rd = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(getAllDocs())));
 
-		int count1 = 0;
 		String line;
+		
+		int count = -1;
+		int count1 = 0;
+		
+		Integer noNodes = Grid.getGridSize();		
 		
 		while((line = rd.readLine())!=null)	{
 			
-			Integer rtrId = nodeSelecter.nextInt(Grid.getGridSize());
-			DataPacket pac = new DataPacket(line,rtrId,0);
+			count++;
+			
+			//Integer rtrId = nodeSelecter.nextInt(Grid.getGridSize());
+			DataPacket pac = new DataPacket(line, count % noNodes, 0);
 			
 			if (SimulationController.getObjectSegmentation() == SimulationTypes.SIMULATION_SEG_ON) {
 				size [count1] = (long)pac.getSizeOfPacket();
@@ -161,7 +162,9 @@ public class PacketDistributions {
 			
 			count1++;
 			
-			CCNRouter router = Grid.getRouter(rtrId);
+			pac.setSegmentId(0);
+			
+			CCNRouter router = Grid.getRouter(count % noNodes);
 			CCNCache routerLocalCache = router.getLocalStorage();
 			pac.setLocality(true);
 			routerLocalCache.addToCache(pac);
@@ -179,8 +182,7 @@ public class PacketDistributions {
 	
 	/* We distribute content between the leaf nodes of 100 and 39 custom-built topologies. The leaf nodes are selected in circular
 	 * list starting from the node having smallest ID. We select the objects from a given docs.web file. */
-	private static void distributeContentGlobeTrafficLeafNodes() throws IOException {
-		
+	private static void distributeContentGlobeTrafficLeafNodes() throws IOException {		
 		
 		/* Manually enter leaf nodes based on the topology created in Brite for 100 nodes using GlobeTraff*/
 		
@@ -204,8 +206,7 @@ public class PacketDistributions {
 			
 			leafNodes = new ArrayList <Integer> (31);
 			for (int i = 8; i <= 38; i++)	
-				leafNodes.add(i); 
-			
+				leafNodes.add(i); 			
 		}
 		
 		else {
@@ -226,7 +227,7 @@ public class PacketDistributions {
 			
 			count++;
 			
-			DataPacket pack = new DataPacket(line,PacketDistributions.leafNodes.get(count % noNodes),0);
+			DataPacket pack = new DataPacket(line, PacketDistributions.leafNodes.get(count % noNodes), 0);
 			
 			System.out.println ("Data object " + count + " will reside in Node " + PacketDistributions.leafNodes.get(count % noNodes));
 			
